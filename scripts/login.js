dc2dd8cf4398f4745832595245d2e384911e13ca
Lavrentijs <llembersk@gmail.com>
2018-11-23 08:06:53 +0200

    design configuration

--- scripts/login.js
+++ scripts/login.js
@@ -1,7 +1,7 @@
 function readInputValues(event) {
     event.preventDefault();
-    let user = document.getElementById("username1");
-    let pass = document.getElementById("password1");
+    let user = document.getElementById("usernameInput");
+    let pass = document.getElementById("passwordInput");
     console.log('Username - ' + user.value + ' , Password - ' + pass.value);
     window.location.href = 'mainMenuProperties.html';
 }
