0828b04c87217af9ac5fea5a04f6f59fadfed201
LavrL <llembersk@gmail.com>
2019-03-22 15:25:50 +0200

    updated propertiesPage

---
+++ scripts/app.js
@@ -0,0 +1,108 @@
+﻿function createElement(tag, properties, ...children) {
+    //console.log('tag - ' + tag, ' properties - ' + properties, 'children -  ' + children)
+
+    const element = document.createElement(tag);
+    //console.log('element ' + element);
+
+    Object.keys(properties).forEach(key => {
+        element[key] = properties[key]
+        //console.log('element - ' + element, 'key - ' + key)
+    });
+
+    if (children.length > 0) {
+        children.forEach(child => {
+            if (typeof child === 'string') {
+                //console.log('CHILD - ' + child);
+                child = document.createTextNode(child);
+            }
+            element.appendChild(child);
+        });
+    }
+
+    return element;
+}
+
+function createTodoItem(title) {
+    //const checkbox = createElement('input', { type: 'checkbox', className: 'checkbox' });
+    const label = createElement('label', { className: 'title' }, title);
+    const editInput = createElement('input', { type: 'text', className: 'textfield' });
+    const editButton = createElement('button', { className: 'edit' }, 'Edit');
+    const deleteButton = createElement('button', { className: 'delete' }, 'Delete');
+    const listItem = createElement('li', { className: 'todo-item' }, label, editInput, editButton, deleteButton);
+
+    bindEvents(listItem);
+
+    return listItem;
+}
+
+function bindEvents(todoItem) {
+    //const checkbox = todoItem.querySelector('.checkbox');
+    const editButton = todoItem.querySelector('.edit');
+    const deleteButton = todoItem.querySelector('.delete');
+
+    //checkbox.addEventListener('change', toggleTodoItem);
+    editButton.addEventListener('click', editTodoItem);
+    deleteButton.addEventListener('click', deleteTodoItem);
+}
+
+function addTodoItem(event) {
+    event.preventDefault();
+
+    if (addInput.value === '') return alert('Nothing was entered');
+
+    const todoItem = createTodoItem(addInput.value);
+
+    todoList.appendChild(todoItem);
+    addInput.value = '';
+}
+
+function toggleTodoItem() {
+    const listItem = this.parentNode;
+    listItem.classList.toggle('completed');
+}
+
+function editTodoItem() {
+    const listItem = this.parentNode;
+    const title = listItem.querySelector('.title');
+    const editInput = listItem.querySelector('.textfield');
+    const isEditing = listItem.classList.contains('editing');
+
+    if (isEditing) {
+        //oldAddress = editInput.value;
+        title.innerText = editInput.value;
+        //console.log('Saving - ' + title.innerText)
+        //console.log('oldAddress - ' + oldAddress)
+        //console.log('title.innerText - ' + title.innerText)
+        console.log('Savin in DB - ' + editUserAddress(oldAddress, title.innerText))
+        this.innerText = 'Edit';
+    }
+    else {
+        editInput.value = title.innerText;
+        oldAddress = editInput.value;
+        console.log('Editing text - ' + oldAddress)
+        this.innerText = 'Save';
+    }
+
+    listItem.classList.toggle('editing');
+}
+
+function deleteTodoItem() {
+    const listItem = this.parentNode;
+    //console.log(listItem);
+    console.log(listItem.getElementsByClassName("title")[0].innerHTML);
+    todoList.removeChild(listItem);
+    console.log('Deleted address - ' + deleteUserAddress(listItem.getElementsByClassName("title")[0].innerHTML))
+}
+
+const todoForm = document.getElementById('todo-form');
+const addInput = document.getElementById('add-input');
+const todoList = document.getElementById('todo-list');
+const todoItems = document.querySelectorAll('.todo-item');
+var oldAddress = '';
+
+function main() {
+    todoForm.addEventListener('submit', addTodoItem);
+    todoItems.forEach(item => bindEvents(item));
+}
+
+main();
--- scripts/login.js
+++ scripts/login.js
@@ -29,6 +29,7 @@ function login() {
     let url = "https://e-services-backend.herokuapp.com/v1/me";
     let username = document.getElementsByName("username")[0].value;
     let password = document.getElementsByName("password")[0].value;
+    var userAddreses = [];
     let headers = new Headers();
     headers.append("Authorization", make_base_auth(username, password));
     headers.append("Content-Type", "application/json");
@@ -40,15 +41,21 @@ function login() {
         .then(response => {
             console.log("ok");
             response.json().then((data) => {
-                console.log('name - ' + data.content.name);
+                //console.log(JSON.parse(sessionStorage.getItem('userAddreses')))
+                //console.log('user ID - ' + data.content.id);
+
                 sessionStorage.setItem("name", data.content.name);
                 sessionStorage.setItem("password", document.getElementsByName("password")[0].value)
+                sessionStorage.setItem("userID", data.content.id)
+                //console.log('user address - ' + data.content.address)
             })
 
             sessionStorage.setItem("session", document.getElementsByName("username")[0].value);
 
             console.log('session ' + sessionStorage.getItem('session'));
             console.log('session name ' + sessionStorage.getItem('name'))
+            console.log('userID ' + sessionStorage.getItem('userID'))
+
             window.location.href = "profilePage.html"
         })
         .catch(error => {
@@ -101,4 +108,141 @@ function returnToIndex() {
 
 function saveCredentials() {
     document.getElementsByClassName("menuBar__credentialsSave_black")[0].innerHTML = "Data was updated";
+}
+
+function fetchUserAddreses() {
+    let url = "https://e-services-backend.herokuapp.com/v1/me/properties";
+    let username = sessionStorage.getItem('name');
+    let password = sessionStorage.getItem('password');
+    var userAddreses = [];
+    let headers = new Headers();
+    headers.append("Authorization", make_base_auth(username, password));
+    headers.append("Content-Type", "application/json");
+
+    fetch(url, {
+        method: "GET",
+        headers: headers,
+    }).then(handleErrors)
+        .then(response => {
+            console.log("ok");
+            response.json().then((data) => {
+                for (let i = 0; i < data.content.length; i++) {
+                    console.log('address - ' + JSON.stringify(data.content[i].address));
+                    userAddreses[i] = data.content[i].address
+                    sessionStorage.setItem("userAddreses", JSON.stringify(userAddreses));
+                }
+                var userAddreses1 = JSON.parse(sessionStorage.getItem("userAddreses"));
+                for (let i = 0; i < userAddreses1.length; i++) {
+                    const todoItem = createTodoItem(userAddreses1[i]);
+                    todoList.appendChild(todoItem);
+                }
+                console.log(JSON.parse(sessionStorage.getItem('userAddreses')))
+            })
+
+            //sessionStorage.setItem("session", document.getElementsByName("username")[0].value);
+            //console.log('session ' + sessionStorage.getItem('session'));
+            //console.log('session name ' + sessionStorage.getItem('name'))
+            //console.log('userID ' + sessionStorage.getItem('userID'))
+            // window.location.href = "profilePage.html"
+        })
+        .catch(error => {
+            console.log(error);
+            //document.getElementsByClassName("input-form__error-message")[0].innerHTML = "Incorrect Username or Password";
+            //let loginForm = document.getElementsByClassName('input-form')[0];
+            //loginForm.reset();
+        })
+}
+
+function addUserAddress() {
+    let url = "https://e-services-backend.herokuapp.com/v1/me/properties";
+    let username = sessionStorage.getItem('name');
+    let password = sessionStorage.getItem('password');
+    //var userAddreses = [];
+    let headers = new Headers();
+
+    headers.append("Authorization", make_base_auth(username, password));
+    headers.append("Content-Type", "application/json");
+
+    //const addInput = document.getElementById('add-input');
+
+    fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({
+            "address": addInput.value,
+            "user_id": sessionStorage.getItem('userID')
+        })
+    }).then(handleErrors)
+        .then(response => {
+            console.log("ok");
+            response.json().then((data) => {
+                console.log(JSON.parse(sessionStorage.getItem('userAddreses')))
+            })
+        })
+        .catch(error => {
+            console.log(error);
+
+        })
+}
+
+function deleteUserAddress(addressToDelete) {
+    let url = "https://e-services-backend.herokuapp.com/v1/me/properties/" + addressToDelete;
+    let username = sessionStorage.getItem('name');
+    let password = sessionStorage.getItem('password');
+    //var userAddreses = [];
+    let headers = new Headers();
+
+    headers.append("Authorization", make_base_auth(username, password));
+    headers.append("Content-Type", "application/json");
+
+    //const addInput = document.getElementById('add-input');
+
+    fetch(url, {
+        method: "DELETE",
+        headers: headers,
+
+    }).then(handleErrors)
+        .then(response => {
+            console.log("ok");
+            response.json().then((data) => {
+                //console.log(JSON.parse(sessionStorage.getItem('userAddreses')))
+            })
+        })
+        .catch(error => {
+            console.log(error);
+
+        })
+}
+
+function editUserAddress(oldAddress, newAddress) {
+    let url = "https://e-services-backend.herokuapp.com/v1/me/properties/" + newAddress;
+    let username = sessionStorage.getItem('name');
+    let password = sessionStorage.getItem('password');
+    //var userAddreses = [];
+    let headers = new Headers();
+
+    headers.append("Authorization", make_base_auth(username, password));
+    headers.append("Content-Type", "application/json");
+
+    console.log('oldAddress -  ' + oldAddress)
+    //const addInput = document.getElementById('add-input');
+
+    fetch(url, {
+        method: "PUT",
+        headers: headers,
+        body: JSON.stringify({
+            "oldAddress": oldAddress,
+            "user_id": sessionStorage.getItem('userID')
+        })
+    }).then(handleErrors)
+        .then(response => {
+            console.log("ok");
+            response.json().then((data) => {
+                //console.log(JSON.parse(sessionStorage.getItem('userAddreses')))
+            })
+        })
+        .catch(error => {
+            console.log(error);
+
+        })
 }
